8fe8bc3ad94feca7855762621bff41e27c3f24ad
t-jinjin <hjinjin06@gmail.com>
2022-12-10 01:27:10 +0900

    Initial Commit

---
+++ Algebra/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algebra
+//
+//  Created by Takuma Jinnai on 2022/12/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
