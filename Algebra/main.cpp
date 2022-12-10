52b50daa4ae6f2093886631b55dfad7fcae4a597
t-jinjin <hjinjin06@gmail.com>
2022-12-11 00:47:02 +0900

    1st commit.

--- Algebra/main.cpp
+++ Algebra/main.cpp
@@ -6,9 +6,89 @@
 //
 
 #include <iostream>
+#include <memory>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+class Set{
+public:
+    virtual bool operator==(const Set&) const = 0;
+    virtual bool operator!=(const Set&) const = 0;
+    virtual Set &operator=(const Set&) = 0;
+};
+
+class AbelianGroup : public Set{
+public:
+    //加法演算の定義
+    
+    virtual AbelianGroup& operator+(const AbelianGroup&) = 0;
+    //逆元の定義
+    virtual const AbelianGroup& operator-() const = 0;
+    //単位元を返す関数の定義
+    virtual const AbelianGroup& identity() const = 0;
+};
+
+class Integer : public AbelianGroup{
+public:
+    Integer(int val){
+        value_ = val;
+    }
+    ~Integer(){
+        //delete this;
+    }
+    
+    bool operator==(const Set& rhs) const override{
+        const Integer rhs_cast = static_cast<const Integer&>(rhs);
+        return value_ == rhs_cast.value_;
+    }
+    bool operator!=(const Set& rhs) const override{
+        const Integer rhs_cast = static_cast<const Integer&>(rhs);
+        return !(*this == rhs_cast);
+    }
+    Integer &operator=(const Set& rhs) override{
+        const Integer& rhs_cast = static_cast<const Integer&>(rhs);
+        value_ = rhs_cast.value_;
+        return *this;
+    }
+    
+    Integer& operator+(const AbelianGroup& rhs) override{
+        const Integer& rhs_cast = static_cast<const Integer&>(rhs);
+        int res = value_ + rhs_cast.value_;
+        return *new Integer(res);
+    }
+    
+    Integer& operator-() const override{
+        return *new Integer(-value_);
+    }
+    
+    Integer& identity() const override{
+        return *new Integer(0);
+    }
+    
+    int value(){
+        return value_;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const Integer& rhs){
+        os << rhs.value_;
+        return os;
+    }
+    
+private:
+    int value_;
+};
+
+
+
+int main(){
+    Integer a = 5;
+    Integer b = 6;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "a + a = " << a + a << endl;
+    cout << "a + (-a)=" << a + (-a) << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "a + (-b)=" << a + (-b) << endl;
     return 0;
 }
